38cf31b17c268996f7508077e686e1d912d01d73
Javier <jmartin@grupoloyal.com>
2019-10-10 20:24:37 +0200

    notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoggerService } from 'src/mp-core';
+import { NotificationService } from './common-app/notification.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,18 @@ import { LoggerService } from 'src/mp-core';
 })
 export class AppComponent {
   title = 'Hola mundo';
-  constructor(out: LoggerService) {
-    out.error('Esto es un error');
-    out.warn('Esto es un warn');
-    out.info('Esto es un info');
-    out.log('Esto es un log');
-  }
+  // constructor(out: LoggerService) {
+  //   out.error('Esto es un error');
+  //   out.warn('Esto es un warn');
+  //   out.info('Esto es un info');
+  //   out.log('Esto es un log');
+  // }
+
+  // constructor(notify: NotificationService) {
+  //   notify.add('Esto es un notificacion');
+  //   notify.delete(0);
+  //   notify.add(null);
+  //   notify.delete(0);
+
+  // }
 }
--- src/app/common-app/notification.service.ts
+++ src/app/common-app/notification.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from 'src/mp-core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  private listado: Array<string> = [];
 
-  constructor() { }
+  constructor(private out: LoggerService) { }
+
+  public get Listado() { return Object.assign([], this.listado); }
+  public get HayNotificaciones(): boolean { return this.listado.length > 0; }
+
+  public add(msg: string): void {
+    if (!msg) {
+      this.out.error('Falta el mensaje de notificacion');
+      return;
+    }
+    this.listado.push(msg);
+    this.out.warn(`NOTIFICACION: ${msg}`);
+  }
+  public delete(index: number) {
+    if (0 <= index && index < this.listado.length) {
+      this.listado.splice(index, 1);
+    } else {
+      this.out.error('Index out of range');
+    }
+  }
+  public clear() {
+    if (this.HayNotificaciones) {
+      this.listado.splice(0);
+    }
+  }
 }
