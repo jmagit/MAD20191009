101ceb8701dfc64e4fa53f599c2bdbcf1cec3e70
Javier <jmartin@grupoloyal.com>
2019-10-14 16:09:33 +0200

    Ampliación

--- src/app/common-app/notification.service.ts
+++ src/app/common-app/notification.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
 import { LoggerService } from 'src/mp-core';
 
+export enum NotificationType { error, warn, info, log }
+
+export class Notification {
+  constructor(private id: number, private msg: string, private type: NotificationType) {}
+  public get Id() { return this.id; }
+  public set Id(valor: number) { this.id = valor; }
+  public get Message() { return this.msg; }
+  public set Message(valor: string) { this.msg = valor; }
+  public get Type() { return this.type; }
+  public set Type(valor: NotificationType) { this.type = valor; }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private listado: Array<string> = [];
+  private listado: Array<Notification> = [];
 
   constructor(private out: LoggerService) { }
 
   public get Listado() { return Object.assign([], this.listado); }
   public get HayNotificaciones(): boolean { return this.listado.length > 0; }
 
-  public add(msg: string): void {
+  public add(msg: string, type = NotificationType.error) {
     if (!msg) {
-      this.out.error('Falta el mensaje de notificacion');
+      this.out.error('Falta el mensaje');
       return;
     }
-    this.listado.push(msg);
-    this.out.warn(`NOTIFICACION: ${msg}`);
+    const id = this.HayNotificaciones ? this.listado[this.listado.length - 1].Id + 1 : 1;
+    this.listado.push(new Notification(id, msg, type));
+    if (type === NotificationType.error) {
+      this.out.error(`NOTIFICACION: ${msg}`);
+    }
   }
-  public delete(index: number) {
+  public remove(index: number) {
     if (0 <= index && index < this.listado.length) {
       this.listado.splice(index, 1);
     } else {
